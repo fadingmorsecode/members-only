a2c960e6780bac3f3418929db0f0ce90a1ac791d
fadingmorsecode <06215095+fadingmorsecode@users.noreply.github.com>
2025-03-23 14:30:10 -0700

    create database script

---
+++ config/createDb.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  firstname VARCHAR (255), lastname VARCHAR(255), username VARCHAR ( 255 ), password VARCHAR (255), membership BOOLEAN DEFAULT false
+)`;
+
+async function main() {
+  console.log('seeding...');
+  const client = new Client({
+    connectionString: process.env.CONNECTIONSTRING,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('done');
+}
+
+main();
