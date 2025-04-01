50189384405b5bb4c60b6112c7e2b4760ddd809b
fadingmorsecode <06215095+fadingmorsecode@users.noreply.github.com>
2025-03-31 23:25:33 -0700

    fix connection string

--- config/queries.js
+++ config/queries.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://dwill:Mrmoomoo1@localhost:5432/members_only',
+  connectionString: process.env.CONNECTIONSTRING,
 });
 
 async function createUser(firstname, lastname, email, password) {
