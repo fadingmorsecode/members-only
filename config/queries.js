44c36d8379face7f4d1aafe40b5ea6a440ad02df
fadingmorsecode <106215095+fadingmorsecode@users.noreply.github.com>
2025-04-01 05:22:59 -0700

    try again

--- config/queries.js
+++ config/queries.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString:
     'postgresql://neondb_owner:npg_p24qQWBnUviC@ep-noisy-frost-a5sp669h-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
+  ssl: { rejectUnauthorized: false },
 });
 
 async function createUser(firstname, lastname, email, password) {
